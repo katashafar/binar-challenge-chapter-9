0e90057c0e28a35eac85314955918f3b43b65b91
Shafar Yulianto Nugroho <45268908+katashafar@users.noreply.github.com>
2022-10-27 00:27:50 +0700

    change firebase config & app js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useState, useEffect } from "react";
 import "./App.css";
-
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      console.log(data);
+    };
+    getUsers();
+  }, []);
   return <div className="App"></div>;
 }
 
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXa-lp0rSNLtSI7wzWo4ZcqtK-K_3Dv2k",
-  authDomain: "chapter-9-b7e00.firebaseapp.com",
-  projectId: "chapter-9-b7e00",
-  storageBucket: "chapter-9-b7e00.appspot.com",
-  messagingSenderId: "857505671924",
-  appId: "1:857505671924:web:975abc61a80e758d030ca1",
-  measurementId: "G-R07PT89MW1",
+  apiKey: "AIzaSyA-DEQ5OCy1T7F3elfyOL7SHMQgTsOOekU",
+  authDomain: "chapter9-78aef.firebaseapp.com",
+  projectId: "chapter9-78aef",
+  storageBucket: "chapter9-78aef.appspot.com",
+  messagingSenderId: "583058089391",
+  appId: "1:583058089391:web:1d16046229ac66ea8f3b5a",
+  measurementId: "G-0BTYLVSPLB",
 };
 
 const app = initializeApp(firebaseConfig);
