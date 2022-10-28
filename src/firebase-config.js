0ddbcf3e5a2826b832182ee1d70f992a82108552
Shafar Yulianto Nugroho <45268908+katashafar@users.noreply.github.com>
2022-10-28 14:47:54 +0700

    change api in firebase config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,6 +4,8 @@ import { getFirestore } from "@firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyA-DEQ5OCy1T7F3elfyOL7SHMQgTsOOekU",
   authDomain: "chapter9-78aef.firebaseapp.com",
+  databaseURL:
+    "https://chapter9-78aef-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "chapter9-78aef",
   storageBucket: "chapter9-78aef.appspot.com",
   messagingSenderId: "583058089391",
